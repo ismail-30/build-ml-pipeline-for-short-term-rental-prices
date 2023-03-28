7ec053a2346f7ddb0e35a64c3f97fe3d56af13e8
ismail Arjdal <ismail.arjdal2@gmail.com>
2023-03-28 03:29:52 +0100

    remove geographical outliers in cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -44,6 +44,10 @@ def go(args):
     logger.info("Converting date into datetime format")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("Removing geographical outliers")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Saving dataframe as a csv file")
     df.to_csv("clean_sample.csv", index=False)
 
